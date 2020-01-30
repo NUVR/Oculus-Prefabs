0239306e2893e58d9670d938857793e9af4d87c3
DarkAce65 <tvdood99@gmail.com>
2020-01-29 23:50:48 -0500

    Start rewriting button

---
+++ Assets/HandCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollider : MonoBehaviour
+{
+    public OvrAvatar avatar;
+
+    private bool isAttached = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAttached)
+        {
+            Transform rightIndexTransform = avatar.GetHandTransform(OvrAvatar.HandType.Right, OvrAvatar.HandJoint.IndexTip);
+            if (rightIndexTransform != null)
+            {
+                gameObject.transform.SetParent(rightIndexTransform);
+                gameObject.transform.localPosition = Vector3.zero;
+                gameObject.transform.localRotation = Quaternion.identity;
+                isAttached = true;
+            }
+        }
+    }
+}
